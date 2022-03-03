8c6da6b75795ceacd128d1338135d04ddc971918
McKay H <admin@McKay.local>
2022-03-03 08:13:20 -0700

    connected list name to display

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 let lists = [];
 
 function addNewList(){
-   const given = {
-       name: document.getElementById('listIn').value,
-       Items: [],
-   }
-   lists.push(given);
-   displayList();
+    const list = document.getElementById('listIn').value;
+    if(list != '')
+    {
+
+        const given = {
+            name: list,
+            Items: [],
+        }
+        lists.push(given);
+        displayList();
+    }
 }
 
 function displayList(){
     let listStr = '';
     lists.forEach(index => {
-        listStr += `<li class="listClass" id="${index.name}" onclick="displayToDo()">
+        listStr += `<li class="listClass" id="${index.name}" onclick="displayToDo('${index.name}')">
         <button class=" deleteList mdl-button mdl-js-button mdl-button--icon">
         <i class="material-icons">cancel</i>
         </button>
@@ -25,34 +30,19 @@ function displayList(){
 function addGoal(){
     document.getElementById("textInModal").style.display = "flex";
 }
-
-function addItem(){
-    const newGoal = document.getElementById('whatToDo').value;
-    if(newGoal === '')
-    {
-        return document.getElementById('whatToDo').value = '';
-    }
-    items.push(newGoal);
-    document.getElementById('whatToDo').value = '';
-    displayItems();
-}
-function displayItems(){
-    let goal = '';
-    console.log(items);
-    items.forEach(index => {
-        goal +=`<li class="toDo mdl-list__item">
-                    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect doneButton">Done</button>
-                    <span class="goal mdl-list__item-primary-content">
-                        <button class=" editing mdl-button mdl-js-button mdl-button--icon" onclick="editGoal()">
-                        <i class="material-icons">edit</i>
-                        </button>
-                    <span class="text">${index}</span> 
-                    </span> 
-                    <button class=" deleteButton mdl-button mdl-js-button mdl-button--icon">
-                        <i class="material-icons">cancel</i>
-                    </button>
-                </li>`;
-    });
-    document.querySelector('liItem').innerHTML = goal;
-    document.getElementById("textInModal").style.display = "none";
+function displayToDo(name){
+    document.getElementById('mainContain').innerHTML = `
+        <div class="demo-card-wide mdl-card mdl-shadow--2dp">
+            <div class="mdl-card__title">
+            <h2 class="mdl-card__title-text" id="title">${name}</h2>
+            </div>
+            <div class="mdl-card__actions mdl-card--border">
+            </div>
+            <div class="mdl-card__menu">
+                <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored" onclick="addGoal()">
+                    <i class="material-icons">add</i>
+                </button>
+            </div>
+            <ul class="mdl-list" id="liItem"></ul>
+        </div>`; 
 }
